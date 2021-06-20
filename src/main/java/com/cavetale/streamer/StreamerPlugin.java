95b61a790019c2d1ce0ae6fd21c749744804602d
StarTux <ths.loy@googlemail.com>
2021-06-20 22:30:53 +0200

    Increase disconnects

--- src/main/java/com/cavetale/streamer/StreamerPlugin.java
+++ src/main/java/com/cavetale/streamer/StreamerPlugin.java
@@ -243,7 +243,7 @@ public final class StreamerPlugin extends JavaPlugin implements Listener {
             if (streamer.getSpectatorTarget() == null) {
                 streamer.setSpectatorTarget(target);
                 spectateLocation = target.getLocation();
-            } else if (spectateLocation != null && tooFar(spectateLocation, streamer.getLocation(), 1024.0)) {
+            } else if (spectateLocation != null && tooFar(spectateLocation, streamer.getLocation(), 256.0)) {
                 streamer.setSpectatorTarget(null);
             }
         }
